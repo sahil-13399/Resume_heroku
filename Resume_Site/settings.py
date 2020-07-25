af00f489609d2f8b6c7b0d454ca4ef120193bae2
sahil <sahil13399@gmail.com>
2020-07-25 20:14:04 +0530

    second_commit

--- Resume_Site/settings.py
+++ Resume_Site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-2hn*fog8(l$#gan0&sbcij2$2x-efc5rkq@dvb()sq(^40a1s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sahil-resume.herokuapp.com','127.0.0.1']
 
 
 # Application definition
